9dac6f7abaa7e62df3d0abc0ed9b7d56b041c943
davidfastgreene <dgreen88@uncc.edu>
2016-12-11 22:39:55 -0500

    Fixed board, used a friends validation method, board is displaying correct tokens, continue working on; 2d array, simplicity of validation, and column height

--- src/connect_four_client_server/Client.java
+++ src/connect_four_client_server/Client.java
@@ -1,72 +1,126 @@
 package connect_four_client_server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Client {
 
 	public static Socket client; //players
     public static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in)); //track user input
     public static PrintWriter outServer; //to write
-    public static BufferedReader inputServer; //to read
+    public static DataInputStream serverInput;
+    public static DataOutputStream serverOutput;
     public static final String HOST = "localhost"; //localhost
     public static final int PORT = 80; //port number
     public static int input; //takes user input
     public static String is; //input receiver
     public static Boolean x;
-    public static int pl1num = 0;
-	
+    public static char[] symbol = new char[] { 'X', 'O' };
+    public static Scanner scan = new Scanner(System.in);
+    
+    
 public static void main(String[] args){
-    	
+	
         System.out.println("Connecting to server..."); //update message
         try{
 			client = new Socket(HOST, PORT); //IP and Port number
-			inputServer = new BufferedReader(new InputStreamReader(client.getInputStream())); //read input from server
-			outServer = new PrintWriter(client.getOutputStream(), true); //if connections exists, write to server
-			System.out.println("Connection successful to SERVER!");
+			serverInput = new DataInputStream(client.getInputStream()); //read input from server
+			serverOutput = new DataOutputStream(client.getOutputStream());
 			
-			for(int i = 0; i < 7; i++){
-				is = inputServer.readLine(); //receives input from server
-			    System.out.println(is); //displays input from server
-			}
-			try{
-				
-				do{
-				    System.out.println("Enter your move(COLUMN 1-7): "); //asks for column
-				    input = keyboard.read(); //gathers input from user
-			        outServer.println(input); //send user input to server
-				}while((input < 1 || input > 6));
-				//method();
-			}catch(InputMismatchException e){
-		        System.out.println("THIS IS NOT A VALID MOVE"); //if letter or special characters are input
-		        //keyboard.readLine();
-		    }
+			System.out.println("Connection successful to SERVER!");
+			System.out.println("Connect Four");//column message
+			
+			int player = serverInput.readInt();
+			
+			Server.board(); //calls board method in Server class
+			System.out.println("Player " + player); //read player
+			
+			  String move;		// # of column to drop piece into
+		      int col;			// Value of move as an int
+		      int currentPlayer;	// Current player's turn
+		      boolean valid;		// Determines if the input from the user can be a valid move
+		        
+			
+//			//DISPLAY BOARD
+//			for(int i = 0; i < 6; i++){
+//				for(int c = 0; c < 7; c++){
+//					
+//					System.out.print(serverInput.readChar()); //displays input from server
+//				}
+//				System.out.println("");
+//			}
+		      
+		      
+		      for (int i=0; i<42; i++) {
+	            	// Determine's who's turn it is (passed in from server)
+	            	currentPlayer = serverInput.readInt();
+	            	
+	            	// Takes a turn
+	            	if (currentPlayer == player) {
+	            		valid = false;	// Makes valid false at the beginning of each turn
+	            		while (valid == false) {
+	                		System.out.println("Enter a column  0-6: ");
+	                		move = scan.nextLine();		// Gets input from user as a String
+	                		if (move.matches("[1-7]{1}") == true) {		// Checks to see if input is a valid column
+	                			col = Integer.parseInt(move)-1;
+	                			if (Server.columnHeight(col) < 6) {	// Checks to see if column entered is full
+	                				Server.update(col, 'X');		// Updates current player's board
+	                				serverOutput.writeInt(col);			// Sends move to server to update other player's board
+	                				System.out.println("Sending move to server.");
+	                				valid = true;	// Makes valid true to end loop
+	                			} else {
+	                				System.out.println("Column " + (col+1) + " is full.");
+	                				valid = false;	// Makes valid false to loop until a legal move is entered
+	                			}
+	                		} else {
+	                			System.out.println("Enter a column between 1 and 7.");
+	            				valid = false;		// Makes valid false to loop until a legal move is entered
+	                		}
+	                    }  //loops while move is invalid	
+	            	} else {
+	            		// Lets other player go
+	            		System.out.println("Waiting for other player");
+	            		col = serverInput.readInt();
+	            		Server.update(col, 'O');
+	            		System.out.println("Other player moved in column " + (col+1) + ".");
+	            	}           	
+	            	Server.board();	// Prints board after a move by either player is made
+	            }
+		      
+		      
+		      
+		      
+		      
+		      
+//			try{
+//				do{
+//					System.out.println("Player " + symbol);
+//				    input = keyboard.read(); //input from user
+//				    
+//				    if((input >=0 && input <=6)){
+//				    	System.out.println("THIS IS NOT A VAaaLID MOVE");
+//				    	continue;}
+//				    
+//				    for(int i = 0; i < 6; i++){
+//						for(int c = 0; c < 7; c++){
+//							System.out.print(serverInput.readChar()); //displays input from server
+//						}
+//						System.out.println("");
+//					}
+//				    
+//				}while((input < 1 || input > 6));
+//				//method();
+//			}catch(InputMismatchException e){
+//		        System.out.println("THIS IS NOT A VALID MOVE"); //if letter or special characters are input
+//		        //keyboard.readLine();
+//		    }
             
         }catch(IOException IOex){
             System.err.println("Error connecting to server."); //if can not connect to the server
         }
     }
 
-public static void method () throws IOException {
-    
-	try{
-		do{
-			System.out.print("Enter your move: ");
-	        
-	        input = keyboard.read(); //gathers input from user
-	        if(input < 1 || input > 6){System.out.println("Enter a valid column");}
-	        outServer.println(input); //send user input to server
-	        //is = inputServer.readLine(); //receives input from server
-	        //System.out.println(is); //displays input from server
-		}while(true); 
-        }catch(InputMismatchException e){
-            System.out.println("THIS IS NOT A VALID MOVE"); //if letter or special characters are input
-            //keyboard.readLine();
-            }   
-	}
-	
+		
 }
--- src/connect_four_client_server/Server.java
+++ src/connect_four_client_server/Server.java
@@ -5,119 +5,119 @@ package connect_four_client_server;
  */
 
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Server {
 	
 	//Create a public static PORT and IP for the server.
+	
     final static int PORT = 80; //port number
     public static Socket client1; //connection to server
     public static Socket client2; //connection to server
     public static ServerSocket server; //server waits for connection
-    public static PrintWriter outClient1;
-    public static PrintWriter outClient2;
     public static DataOutputStream client1writer;
     public static DataOutputStream client2writer;
-    public static BufferedReader inputClient1;
-    public static BufferedReader inputClient2;
-    public static boolean c = true; //loop variable
-    public static String line = null;
-    public static List<String> temps = new ArrayList<String>(); //temp list array for the board
-    public static String [][] board = new String[6][7]; //board array
+    public static DataInputStream client1reader;
+    public static DataInputStream client2reader;
     
+    public static int ROWS = 6;
+    public static int lastCol = -1, lastTop = -1;
+    public static int COLS = 7;
+    public static char[][] board = new char[ROWS][COLS];
+	public static int[] columnHeight = new int[COLS]; //height of column
+	
     
     public static void main(String[] args) throws IOException{
     	
         System.out.println("Connecting to client../."); //update message
-        server = new ServerSocket(PORT);
-        
-        //connect four layout
-		BufferedReader bufferedReader = 
-				new BufferedReader(new FileReader("src/connect_four_client_server/c4"));
+        server = new ServerSocket(PORT); //server socket
         
         try{
 			client1 = server.accept(); //accepts connection to client1, player1
 			System.out.println("Connection successful to PLAYER1"); //update message
-            client1writer = new DataOutputStream(client1.getOutputStream());
-          
+            client1writer = new DataOutputStream(client1.getOutputStream()); //to write to client
+            client1reader = new DataInputStream(client1.getInputStream()); //to read from client
+            
 			client2 = server.accept(); //accepts connection to client2, player2
 			System.out.println("Connection successful to PLAYER2"); //update message
-			client2writer = new DataOutputStream(client2.getOutputStream());
+			client2writer = new DataOutputStream(client2.getOutputStream()); //to write to client
+			client2reader = new DataInputStream(client2.getInputStream()); //to read from client
 			
-			//for reading and writing
-			outClient1 = new PrintWriter(client1.getOutputStream(), true); //to write to the client
-			inputClient1 = new BufferedReader(new InputStreamReader(client1.getInputStream())); //to read from client
+			//assign players
+			int PL1 = 1, PL2 = 2;
+			client1writer.writeInt(PL1); client2writer.writeInt(PL2);
 			
-			//for reading and writing
-			outClient2 = new PrintWriter(client2.getOutputStream(), true); //to write to the client
-			inputClient2 = new BufferedReader(new InputStreamReader(client2.getInputStream())); //to read from client
+			int col, playerCount = 0;
 			
-			//****************************************
-			/*WHEN BOARD IS /POPULATED AND DISPLAYED*/
-			//****************************************
+			for (int i=0; i<42; i++) {
+				//using mod to determine which player is next
+            	client1writer.writeInt((playerCount % 2) + 1); 
+            	client2writer.writeInt((playerCount % 2) + 1);
+            	
+            	//Server output for which player is waiting and what move they make
+            	if (playerCount%2 == 0) {
+            		System.out.println("Waiting for player 1 to make a move...");
+            		col = client1reader.readInt(); //reads input for column         		
+            		update(col, 'X'); //calls the update method
+            		System.out.println("Player 1 moved to " +(col+1));
+            		client2writer.writeInt(col); //updates player 2 board
+            	} else {
+            		System.out.println("Waiting for player 2 to make a move...");
+            		col = client2reader.readInt(); //reads input for column
+            		update(col, 'O'); //calls the update method
+            		System.out.println("Player 2 moved to " + (col+1));
+            		client1writer.writeInt(col); //updates player 1 boards
+            	}
+            	playerCount++; //increments players turn
+            }
 			
-			//assigning values to players
-			String PL1 = "X";
-			String PL2 = "O";
-			
-			//representing each client as players
-			client1writer.writeChars(PL1); client2writer.writeChars(PL2);
-			//display when players have connected
-			outClient1.println(PL1 + " Player 1 has connected"); outClient2.println(PL2 + " Player 2 has connected");
-			
-			
-			//output the c4 board
-			while((line = bufferedReader.readLine()) != null) {temps.add(line);} 
-			
-            bufferedReader.close(); //
-            
-            //String array to display the board
-            String[] tempsArray = temps.toArray(new String[0]); 
-            
-			//populate the board with '-'
-			for (int row = 0; row < 6; row++) { 
-				for (int col = 0; col < 5; col++) { 
-					board[row][col] = tempsArray[col]; 
-					outClient1.println(board[row][col]);
-					outClient2.println(board[row][col]);
-				} 
-			}
-            //********************************
-			/*WHEN COLUMN NUMBER IS RECIEVED*/
-			//********************************
-			
-			int input = inputClient1.read(); //user input column
-			
-			//Update board with new input
-			for (int row = 0; row < 6; row++) { 
-				for (int col = 0; col < 8; col++) { 
-					board[row][col] = tempsArray[col]; 
-					outClient1.println(board[row][col]);
-					outClient2.println(board[row][col]);
-				} 
-			}
-			
-			
-			
-			
-			while(c){}
-			
-
-     //Close both sockets when execution is finished.
     }catch (IOException IOex){
-    	System.err.println("Something went wrong"); //output
-        //client.close(); server.close(); outClient1.close(); inputClient.close(); System.exit(0); //closing everything
+    	System.err.println("Something went wrong");
+    	//Closing everything
+        client1.close(); client2.close(); server.close(); client1writer.close(); 
+        client2writer.close(); client1reader.close(); client2reader.close(); System.exit(0); 
     }
         
+}
+    
+    //****************************************
+	/*WHEN BOARD IS POPULATED AND DISPLAYED*/
+	//****************************************
+    public static void board () {
+//    	for (int row = 0; row < 6; row++) {
+//        	for (int col = 0; col < 7; col++) {
+//        		board[row][col] = '-';
+//                client1writer.writeChar(board[row][col]);
+//                client2writer.writeChar(board[row][col]);
+//  			}
+//        }
+    	
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLS; col++) {
+				System.out.print("-" + board[row][col]);
+			}
+			System.out.println();
+		}
+		for (int col = 0; col < COLS; col++)
+			System.out.print("--");
+		System.out.println();
+		System.out.println(" 1|2|3|4|5|6|7 ");
     }
+    
+    //****************************************
+  	/*UPDATES BOARD AFTER MOVE IS MADE*/
+  	//****************************************
+ 	public static void update (int input, char symbol) throws IOException {
+ 		board[(ROWS-1)-columnHeight[input]][input] = symbol; //decrements through ROWS by input(column)
+ 		columnHeight[input]++; //increments column height
+ 	}
+ 	
+ 	//****************************************
+ 	/*RETURNS THE HEIGHT OF EACH COLUMN*/
+ 	//****************************************
+ 	public static int columnHeight (int input) {return columnHeight[input];}
 
 }
